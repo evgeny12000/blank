6543eb9a0124bb697f493e47c3fbf9c525c08044
evgen <evgeny12000@gmail.com>
2024-02-17 16:10:00 +0200

    add

--- api/src/examples/examples.controller.ts
+++ api/src/examples/examples.controller.ts
@@ -5,17 +5,7 @@ export class ExamplesController {
 
     @Get('test')
     async test(@Request() request: any) {
-
-        function testStackLimit(i = 0) {
-            try {
-                return testStackLimit(i + 1);
-            } catch (e) {
-                return i;
-            }
-        }
-
-        console.log(testStackLimit());
-
+       return 'Ой лала !!!';
     }
 
 
